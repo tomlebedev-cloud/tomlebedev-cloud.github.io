3a5ee4449e7b0c99b7eb81fc3082daee9d3ae3e4
tomlebedev-cloud <tom.lebedev@gmail.com>
2026-09-01 19:16:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: puslapis veikia ir be interneto.
    Pakeitus failus butina padidinti VERSIJA - kitaip telefonas rodys sena. */
-const VERSIJA = 'photography-v11';
+const VERSIJA = 'photography-v12';
 const MAX_NUOTRAUKU = 80;   // kiek nuotrauku laikom talpykloje
 
 const APVALKALAS = [
